b0892b2ff64c84a13d003ac4f39cb904d8e50f96
Lindsay Boyden <lindsayboyden@yahoo.com>
2020-05-18 14:19:17 -0600

    heavy update

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,34 @@
+class PagesController < ApplicationController
+  def index
+    puts "in index method"
+    @pages = Page.all # controller interacting with model
+
+    # although we don't code anything, rails automatically grabs this for us
+    # rails is going to look for index.html.erb in the views/pages folder
+  end
+
+  def show
+      # find a page, but we need id
+      @page = Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new
+    # go to folder views/pages/new.html/txt/etc ex
+  end
+
+  def create
+    @page = Page.new(pages_params)
+    # new doesnt write new object to database, whereas create will
+    if @page.save
+      redirect_to pages_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def pages_params
+    params.require(:page).permit(:title, :author, :body)
+  end
+end
